a2a26a67ed85f074c4cedc6a36d899c70c650d80
Tim <tymur.prokhorov@asquad.io>
2025-08-13 18:43:45 +0300

    redundant drop and creating removed, syntax fixed

--- task.sql
+++ task.sql
@@ -1,14 +1,10 @@
-DROP DATABASE ShopDB;
-
-USE ShopDB;
-
 DELIMITER //
 CREATE PROCEDURE get_warehouse_product_inventory(IN warehouse_id INT)
 BEGIN
     SELECT p.Name AS product_name, pi.WarehouseAmount AS amount_in_warehouse
      FROM Products p
      INNER JOIN ProductInventory pi
-     ON p.ID = pi.ProductID;
-     WHERE pi.WarehouseID = warehouse_id
+     ON p.ID = pi.ProductID
+     WHERE pi.WarehouseID = warehouse_id;
 END //
 DELIMITER ;
