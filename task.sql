0a53a9b9682ffebe39657b60bb73cf20ac85cf3f
Hrechukha-Oleksii <Oleksii_Hrechukha@outlook.com>
2026-07-29 15:39:40 +0300

    Solution4

--- task.sql
+++ task.sql
@@ -3,13 +3,13 @@ USE ShopDB;
 -- Create your stored procedure here
 DELIMITER //
 CREATE PROCEDURE get_warehouse_product_inventory(
-    IN ID INT
+    IN warehouse_id INT
 )
 BEGIN
     SELECT ShopDB.Products.Name, ShopDB.ProductInventory.WarehouseAmount  
     FROM ShopDB.Products
     JOIN ShopDB.ProductInventory
     ON ShopDB.Products.ID = ShopDB.ProductInventory.ProductID
-    WHERE ShopDB.ProductInventory.WarehouseID = ID;
+    WHERE ShopDB.ProductInventory.WarehouseID = warehouse_id;
 END //
 DELIMITER ;
