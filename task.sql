a2deeb9b330a995bf7ce24c681dfe81c23a3f836
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-09-02 16:04:40 +0300

    Complete RDA task 5

--- task.sql
+++ task.sql
@@ -1,3 +1,13 @@
 USE ShopDB; 
 
--- Create your stored procedure here
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory(
+    IN _WarehouseID varchar(10)
+)
+BEGIN
+SELECT b.name, a.WarehouseAmount
+FROM ProductInventory AS a
+LEFT JOIN Products AS b ON a.ProductID = b.ID
+WHERE a.WarehouseID=_WarehouseID;
+END //
+DELIMITER ;
