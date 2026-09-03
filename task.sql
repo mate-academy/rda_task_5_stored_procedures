b894c98d3b2f69c4d4bac284e2ace4ccb5928dc6
Olena Nosenko <nosenko.alena@gmail.com>
2026-09-03 12:49:52 +0300

    added stored procedures

--- task.sql
+++ task.sql
@@ -1,3 +1,16 @@
-USE ShopDB; 
+USE ShopDB;
+
+DELIMITER //
 
 -- Create your stored procedure here
+CREATE PROCEDURE get_warehouse_product_inventory (
+    ID INT
+)
+BEGIN
+    SELECT p.Name, pi.WarehouseAmount
+    FROM Products AS p
+    INNER JOIN ProductInventory AS pi ON (pi.ProductID = p.ID)
+    WHERE pi.WarehouseID = ID;
+END //
+
+DELIMITER ;
